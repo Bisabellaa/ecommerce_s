e407e812f0ab009697540a425c399b3940cc4513
Bisabellaa <ibrenda@pioix.edu.ar>
2026-07-11 15:16:22 -0300

    Agregue nuevos templates, para las demas secciones. Modifique las rutas con {% static %}, para que funcionen con Django. Añadi en el archhivo views.py una funcion de vista para cada template. En el archivo urls.py añadi las urls.

--- sillonesrosi/urls.py
+++ sillonesrosi/urls.py
@@ -21,4 +21,13 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('store/', views.store, name='store'),
+    path('signin/', views.signin, name='signin'),
+    path('register/', views.register, name='register'),
+    path('cart/', views.cart, name='cart'),
+    path('product-detail/', views.product_detail, name='product_detail'),
+    path('place-order/', views.place_order, name='place_order'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('search-result/', views.search_result, name='search_result'),
+    path('order-complete/', views.order_complete, name='order_complete'),
 ]
--- sillonesrosi/views.py
+++ sillonesrosi/views.py
@@ -1,4 +1,31 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html') 
+    return render(request, 'home.html')
+
+def store(request):
+    return render(request, 'store.html')
+
+def signin(request):
+    return render(request, 'signin.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def cart(request):
+    return render(request, 'cart.html')
+
+def product_detail(request):
+    return render(request, 'product-detail.html')
+
+def place_order(request):
+    return render(request, 'place-order.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def search_result(request):
+    return render(request, 'search-result.html')
+
+def order_complete(request):
+    return render(request, 'order_complete.html')
